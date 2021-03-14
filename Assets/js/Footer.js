5d6801c22f6b716f145e1db39f2adb14f48d4f87
Apotheosis Mind <64317865+ApotheosisMind@users.noreply.github.com>
2021-03-14 21:33:30 +0530

    Create Files

---
+++ Assets/js/Footer.js
@@ -0,0 +1,8 @@
+var footer = document.getElementById("DateAtFooter");
+
+function ShowFooterData() {
+    var CrrDate = new Date();
+    var CrrYear = CrrDate.getFullYear();
+
+    footer.innerHTML = "© " + CrrYear + " <a href='webiflu.com' class='smooth-transition-normal'>Webiflu</a>, All Rights Reserved";
+}
